c869e32faf269c698f87d2d76f5b762be41515cf
anandvsinh <anandvardhansinh2007@gmail.com>
2026-02-16 23:51:32 +0530

    method

---
+++ src/com/gla/methods/Student.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+public class Student {
+    int rollno;
+    String name;
+    String course;
+    Double cpi;
+    static String college = "GLA University";
+
+        public Student(int rollno, String name, String course, Double cpi){
+            this.name
+        }
+
+    public static void main() {
+        Student s1 = new Student[]
+    }
+}
